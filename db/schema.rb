0e50b7ca14738a68eb1fdbae111cb65c54ee2e9e
William Mantegna <wmantegna@gmail.com>
2017-01-07 19:16:37 -0500

    reconfirm able fix

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170106194126) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
