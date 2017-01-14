5b84b554a27e5182e0f1eb21972376d964a4465f
William Mantegna <wmantegna@gmail.com>
2017-01-13 23:52:14 -0500

    removed unused overrides

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,13 +1,4 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  def new
-    super
-  end
-
-  # POST /resource/confirmation
-  def create
-    super
-  end
 
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
@@ -23,18 +14,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     end
 
     super
-  end
-
-  protected
-
-  # The path used after resending confirmation instructions.
-  def after_resending_confirmation_instructions_path_for(resource_name)
-    super(resource_name)
-  end
-
-  # The path used after confirmation.
-  def after_confirmation_path_for(resource_name, resource)
-    super(resource_name, resource)
   end
 
   private
